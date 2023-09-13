63f620140d86890485b6a8caaf44e6376807a2d6
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-09-13 23:37:54 +0530

    :passport_control:Added Register func

--- App/views.py
+++ App/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Register a new user
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(
+                request, "app/register.html", {"message": "Passwords must match."}
+            )
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            print("User created")
+        except IntegrityError:
+            return render(
+                request,
+                "app/register.html",
+                {"message": "Username already taken."},
+            )
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        # Load registration form
+        return render(request, 'app/register.html')
