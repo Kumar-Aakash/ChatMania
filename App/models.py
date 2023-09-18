dd7b5f4d7c3b797b1572e86fcfe1552da0e69741
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-09-18 18:08:59 +0530

    :card_file_box:Added User Model

--- App/models.py
+++ App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    """username(str), email(str), password(str), joinedRooms(int,default=0), createdRooms(int,default=0)"""
+    joinedRooms = models.IntegerField(default=0)
+    createdRooms = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username}"
+    
