5e4ae8eedfb084f1dd02a13ed3fd484b8c4e320f
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-09-20 22:05:10 +0530

    :construction_worker:Registered Models to Django Admin

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,12 @@
+
 from django.contrib import admin
+from .models import User, Room, UserRooms, Message, JoinRequests
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(UserRooms)
+admin.site.register(Message)
+admin.site.register(JoinRequests)
+
