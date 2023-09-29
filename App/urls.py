111dca90e2aa625e4b22fba245f9dbd3fedd7830
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-09-29 22:23:40 +0530

    :truck:Added Routes

--- App/urls.py
+++ App/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('create/', views.create, name='create'),
+    path('your_rooms/', views.your_rooms, name='your_rooms'),
+    path('requests', views.join_requests, name='join_requests'),
+    path('joinedRooms', views.joined_rooms, name='joined_rooms'),
+    path('room/<str:room_name>', views.room, name='room'),
+
+   
 ]
